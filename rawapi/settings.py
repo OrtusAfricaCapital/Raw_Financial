962fc145756f2d7f47bbe66a9570a5406a618836
Buhiire Keneth <buhiirekeneth@gmail.com>
2021-12-04 14:48:19 +0300

    add allowed hosts

--- rawapi/settings.py
+++ rawapi/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = 'django-insecure-0=cgbbgmg#2b4m=3i%m@tvllutmk--u$kxglsohntw90c=uay_
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['rawfinancial.fund']
 
 
 # Application definition
