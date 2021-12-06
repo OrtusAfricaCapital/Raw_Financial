1548feaac24cc19d4224f1e4c15e1fe87b9fca36
Joshua <jasiimwe160@gmail.com>
2021-12-07 00:23:26 +0300

    add database

--- rawapi/settings.py
+++ rawapi/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = 'django-insecure-0=cgbbgmg#2b4m=3i%m@tvllutmk--u$kxglsohntw90c=uay_
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['rawfinancial.fund']
+ALLOWED_HOSTS = ['rawfinancial.fund', '127.0.0.1']
 
 
 # Application definition
